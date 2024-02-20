feaa0f017758ec3d0ddc06ee6fb503e9b689280d
Salih Can Yurtkulu <salih.yurtkulu@ar-lx0124.agile-robots.de>
2024-02-20 16:43:13 +0100

    feat: added some dummy animated markers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,50 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import L, { type MapOptions, Marker } from 'leaflet'
-import 'leaflet-routing-machine'
-// import { map, latLng, tileLayer, marker, type MapOptions } from 'leaflet'
+import { onMounted } from "vue";
+import L, { type MapOptions, Marker } from "leaflet";
+import "leaflet-routing-machine";
+import type { LatLng } from "leaflet";
 
 const options: MapOptions = {
   center: L.latLng(51.505, -0.09),
-  zoom: 13
-}
+  zoom: 13,
+};
 
-let demoMap
-let animatedMarker: Marker
+let demoMap;
+// #2e85cbff
 
-const line = L.polyline(
-  [
-    [40.6851, -73.94136],
-    [40.68576, -73.94149],
-    [40.68649, -73.94165]
-  ],
-  {
-    color: '#02929b',
-    weight: 2
-  }
-)
-
-const moveMarker = () => {
-  let arr = line.getLatLngs()
-
-  let lastElem = arr[arr.length - 1]
-  let firstElem = arr[0]
-
-  arr[arr.length - 1] = firstElem
-  arr[0] = lastElem
-}
 onMounted(() => {
-  demoMap = L.map('map', options)
+  demoMap = L.map("map", options);
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(demoMap)
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(demoMap);
 
-  line.addTo(demoMap)
+  const busIcon = L.divIcon({
+    html: '<i class="fa-solid fa-bus fa-2xl" style="color: #2e85cbff;"></i>',
+    className: "busIcon",
+  });
+  const busMarker = L.marker([56.34213143540303, -2.794179122392289], {
+    icon: busIcon,
+  }).addTo(demoMap);
 
-  animatedMarker = L.animatedMarker(line.getLatLngs(), {
-    autoStart: true,
-    interval: 2000
+  L.Routing.control({
+    waypoints: [
+      L.latLng(56.34213143540303, -2.794179122392289),
+      L.latLng(56.33379736952665, -2.81008349458336),
+    ],
   })
-
-  demoMap.addLayer(animatedMarker)
-
-  const group = L.featureGroup([animatedMarker])
-
-  demoMap.fitBounds(group.getBounds())
-
-  // demoMap.fitBounds(group.getBounds())
-  // L.Routing.control({
-  //   waypoints: [L.latLng(51.505, -0.09), L.latLng(51.506, -0.08)],
-  //   routeWhileDragging: true
-  // }).addTo(demoMap)
-})
+    .on("routesfound", (e) => {
+      const routes = e.routes;
+      routes[0].coordinates.forEach((coord: LatLng, idx: number) => {
+        setTimeout(() => {
+          busMarker.setLatLng([coord.lat, coord.lng]);
+        }, 100 * idx);
+      });
+    })
+    .addTo(demoMap)
+    .hide();
+});
 </script>
 
 <template>
