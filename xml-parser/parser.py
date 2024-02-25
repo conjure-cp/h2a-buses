407a916b2d8c5ddbc9bb7d79f159d48a8a45bd0a
Salih Yurtkulu <salih.yurtkulu@Salihs-MBP.fritz.box>
2024-02-25 22:48:01 +0100

    feat: impl xml-parser(WIP), generated test data (.json)

---
+++ xml-parser/parser.py
@@ -0,0 +1,53 @@
+import os
+import xml.etree.ElementTree as ET
+import json
+
+tag_prefix='{http://www.transxchange.org.uk/}'
+
+# Pass this argument from command line? 
+# F028-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill
+# F038-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill
+# F045-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill
+# F099-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill
+# F099S-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill - Serviced Organisations?
+# F040-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill - Serviced Organisations?
+tree = ET.parse(f'{os.getcwd()}/test-data/F045-None--SCFI-FI-2023-08-14-Fife_Timeplan_1408_2310-Transmill.xml')
+
+root = tree.getroot()
+# Get the first RouteSection
+route_links = root.find(f'{tag_prefix}RouteSections').find(f'{tag_prefix}RouteSection').findall(f'{tag_prefix}RouteLink')
+num_links = len(route_links)
+
+bus_stop_locations = []
+for idx, link in enumerate(route_links):
+    mapping = link.find(f'{tag_prefix}Track').find(f'{tag_prefix}Mapping')
+    location = mapping.find(f'{tag_prefix}Location')
+    lat = location.find(f'{tag_prefix}Latitude').text
+    long = location.find(f'{tag_prefix}Longitude').text
+    bus_stop_locations.append([float(lat), float(long)])
+    
+    # Append the last top as well
+    if idx == num_links - 1:
+        last_stop = mapping.findall(f'{tag_prefix}Location')[-1]
+        bus_stop_locations.append([float(last_stop.find(f'{tag_prefix}Latitude').text), 
+                                   float(last_stop.find(f'{tag_prefix}Longitude').text)])
+
+
+# Get Service Code, Origin and Destination info of the bus line
+bus_service = root.find(f'{tag_prefix}Services').find(f'{tag_prefix}Service')
+service_code = bus_service.find(f'{tag_prefix}ServiceCode').text
+line_name = bus_service.find(f'{tag_prefix}Lines').find(f'{tag_prefix}Line').find(f'{tag_prefix}LineName').text
+origin = bus_service.find(f'{tag_prefix}StandardService').find(f'{tag_prefix}Origin').text
+destination = bus_service.find(f'{tag_prefix}StandardService').find(f'{tag_prefix}Destination').text
+
+
+bus_line_details = {
+    "service_code": service_code,
+    "line_name": line_name,
+    "origin": origin,
+    "destination": destination,
+    "stops": bus_stop_locations 
+}        
+
+with open(f"{os.getcwd()}/test-data/json/{service_code}.json", "w+") as outfile:
+    json.dump(bus_line_details, outfile, indent=2)
