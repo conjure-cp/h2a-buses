914be64343212387a08442dd30035cf9593c6505
Salih Yurtkulu <yurtkulu.salih@gmail.com>
2024-03-04 18:10:43 +0100

    feat: (WIP) generate separate RoutingControl objects

--- src/routing/control.ts
+++ src/routing/control.ts
@@ -1,4 +1,46 @@
 import L, { type MapOptions, Marker, LatLng, Layer } from "leaflet";
 import "leaflet-routing-machine";
 
-// export const generateRoutingControl = () 
+const busIcon = L.divIcon({
+  html: '<i class="fa-solid fa-bus fa-2xl" style="color: #2e85cbff;"></i>',
+  className: "busIcon",
+});
+
+export const generateRoutingControl = (waypoints: L.LatLng[], map: L.Map) => {
+  const routingControl = L.Routing.control({
+    waypoints: waypoints,
+  }).on("routesfound", (e) => {
+    routesFoundCallback(e, map);
+  });
+
+  return routingControl;
+};
+
+export const routesFoundCallback = (e: any, map: L.Map) => {
+  let waypointMarkers = new L.FeatureGroup();
+  //set the camera
+  //   const zoomCenter = L.latLng(
+  //     (e.waypoints[0].latLng.lat + e.waypoints[1].latLng.lat) / 2,
+  //     (e.waypoints[0].latLng.lng + e.waypoints[1].latLng.lng) / 2
+  //   );
+  //   map.setView(zoomCenter);
+  //   let zoomThreshold = map.getZoom();
+
+  // add the bus marker to the starting point
+  L.marker([e.waypoints[0].latLng.lat, e.waypoints[0].latLng.lng], {
+    icon: busIcon,
+  }).addTo(map);
+
+  map.eachLayer((layer: Layer) => {
+    if (
+      layer instanceof Marker &&
+      !layer.getIcon().options.className?.includes("bus")
+    ) {
+      waypointMarkers.addLayer(layer);
+    }
+  });
+
+  // TODO: Solve this duplicate layer problem!
+  map.addLayer(waypointMarkers);
+  map.removeLayer(waypointMarkers);
+};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,13 @@
 import { onMounted, ref, watch } from "vue";
 import Card from "@/components/Card.vue";
 import MultiSelect from "primevue/multiselect";
+import { generateRoutingControl } from "@/routing/control";
 import { type BusLine } from "@/utils/types";
 import L, { type MapOptions, Marker, LatLng, Layer } from "leaflet";
 import "leaflet-routing-machine";
 
 let demoMap: L.Map;
-let routingControl: L.Routing.Control | undefined;
-let busMarker: Marker | undefined;
-
-const busIcon = L.divIcon({
-  html: '<i class="fa-solid fa-bus fa-2xl" style="color: #2e85cbff;"></i>',
-  className: "busIcon",
-});
+// let busMarker: Marker | undefined;
 
 const options: MapOptions = {
   center: L.latLng(56.34213143540303, -2.794179122392289),
@@ -23,11 +18,11 @@ const options: MapOptions = {
 const routeOptions = ref<
   {
     label: string;
-    value: number[][];
+    value: LatLng[];
   }[]
 >([]);
-const selectedRoutes = ref([]);
-const availableRoutes: L.LatLng[][] = [];
+const selectedRoutes = ref<LatLng[][]>([]);
+const addedRoutes: L.Control[] = [];
 
 fetch("json/available_lines.json")
   .then((resp) => resp.json())
@@ -39,106 +34,131 @@ fetch("json/available_lines.json")
         .then((resp) => resp.json())
         .then((data: BusLine) => {
           // Collect lat & lng info
-          const route: L.LatLng[] = [];
+          const route: LatLng[] = [];
           data.stops.forEach((elem) => {
             route.push(L.latLng(elem[0], elem[1]));
           });
-          availableRoutes.push(route);
 
           // Collect other info - lineNumber, origin, destination
           routeOptions.value.push({
             label: `${data.line_name} ${data.origin} - ${data.destination}`,
-            value: data.stops,
+            value: route,
           });
         });
     });
   });
 
-const routeCoordinates = ref<LatLng[]>([]);
-const selectedRouteId = ref<number>();
+// TODO: remove this code block
+// const routeCoordinates = ref<LatLng[]>([]);
+// const selectedRouteId = ref<number>();
 
-watch(
-  () => selectedRouteId.value,
-  () => {
-    // If there is a route, remove it from the map
-    if (routingControl !== undefined) {
-      demoMap.removeControl(routingControl);
-      routingControl = undefined;
+// watch(
+//   () => selectedRouteId.value,
+//   () => {
+//     // If there is a route, remove it from the map
+//     if (routingControl !== undefined) {
+//       demoMap.removeControl(routingControl);
+//       routingControl = undefined;
+//     }
+
+//     if (busMarker !== undefined) {
+//       demoMap.removeLayer(busMarker);
+//       busMarker = undefined;
+//     }
+
+//     // calculate the route
+//     if (selectedRouteId.value !== undefined) {
+//       // will be removed
+//       routingControl = L.Routing.control({
+//         waypoints: selectedRoutes.value[selectedRouteId.value as number],
+//       })
+//         .on("routesfound", (e) => {
+//           console.log("event", e);
+//           let busStopMarkers = new L.FeatureGroup();
+//           routeCoordinates.value = e.routes[0].coordinates;
+
+//           // set the camera
+//           const zoomCenter = L.latLng(
+//             (e.waypoints[0].latLng.lat + e.waypoints[1].latLng.lat) / 2,
+//             (e.waypoints[0].latLng.lng + e.waypoints[1].latLng.lng) / 2
+//           );
+//           demoMap.setView(zoomCenter);
+//           let zoomThreshold = demoMap.getZoom();
+
+//           // add the bus marker to the starting point
+//           busMarker = L.marker(
+//             [e.waypoints[0].latLng.lat, e.waypoints[0].latLng.lng],
+//             {
+//               icon: busIcon,
+//             }
+//           ).addTo(demoMap);
+
+//           demoMap.eachLayer((layer: Layer) => {
+//             if (
+//               layer instanceof Marker &&
+//               !layer.getIcon().options.className?.includes("bus")
+//             ) {
+//               busStopMarkers.addLayer(layer);
+//             }
+//           });
+
+//           demoMap.on("zoomend", () => {
+//             // TODO: Solve this duplicate layer problem!
+//             demoMap.addLayer(busStopMarkers);
+//             if (demoMap.getZoom() < zoomThreshold) {
+//               demoMap.removeLayer(busStopMarkers);
+//             }
+//           });
+//         })
+//         .addTo(demoMap);
+
+//       routingControl.hide();
+//     }
+//   }
+// );
+
+// const moveMarker = () => {
+//   if (routeCoordinates.value.length !== 0) {
+//     routeCoordinates.value.forEach((coord: LatLng, idx: number) => {
+//       setTimeout(() => {
+//         busMarker!.setLatLng([coord.lat, coord.lng]);
+//       }, 100 * idx);
+//     });
+//   } else {
+//     alert("Please select a route!");
+//   }
+// };
+
+const findRoutes = () => {
+  // TODO: Existing layers should also be removed
+  demoMap.eachLayer((layer: Layer) => {
+    if (layer instanceof Marker) {
+      demoMap.removeLayer(layer);
     }
+  });
 
-    if (busMarker !== undefined) {
-      demoMap.removeLayer(busMarker);
-      busMarker = undefined;
-    }
-
-    // calculate the route
-    if (selectedRouteId.value !== undefined) {
-      routingControl = L.Routing.control({
-        waypoints: availableRoutes[selectedRouteId.value as number],
-      })
-        .on("routesfound", (e) => {
-          console.log("event", e);
-          let busStopMarkers = new L.FeatureGroup();
-          routeCoordinates.value = e.routes[0].coordinates;
-
-          // set the camera
-          const zoomCenter = L.latLng(
-            (e.waypoints[0].latLng.lat + e.waypoints[1].latLng.lat) / 2,
-            (e.waypoints[0].latLng.lng + e.waypoints[1].latLng.lng) / 2
-          );
-          demoMap.setView(zoomCenter);
-          let zoomThreshold = demoMap.getZoom();
-
-          // add the bus marker to the starting point
-          busMarker = L.marker(
-            [e.waypoints[0].latLng.lat, e.waypoints[0].latLng.lng],
-            {
-              icon: busIcon,
-            }
-          ).addTo(demoMap);
-
-          demoMap.eachLayer((layer: Layer) => {
-            if (
-              layer instanceof Marker &&
-              !layer.getIcon().options.className?.includes("bus")
-            ) {
-              busStopMarkers.addLayer(layer);
-            }
-          });
-
-          demoMap.on("zoomend", () => {
-            // TODO: Solve this duplicate layer problem!
-            demoMap.addLayer(busStopMarkers);
-            if (demoMap.getZoom() < zoomThreshold) {
-              demoMap.removeLayer(busStopMarkers);
-            }
-          });
-        })
-        .addTo(demoMap);
-
-      routingControl.hide();
-    }
+  // Remove every control object added to map first
+  while (addedRoutes.length > 0) {
+    demoMap.removeControl(addedRoutes.shift()!);
   }
-);
 
-const moveMarker = () => {
-  if (routeCoordinates.value.length !== 0) {
-    routeCoordinates.value.forEach((coord: LatLng, idx: number) => {
-      setTimeout(() => {
-        busMarker!.setLatLng([coord.lat, coord.lng]);
-      }, 100 * idx);
+  if (selectedRoutes.value.length !== 0) {
+    selectedRoutes.value.forEach((route: LatLng[]) => {
+      const control = generateRoutingControl(route, demoMap).addTo(demoMap);
+      control.hide();
+      addedRoutes.push(control);
     });
   } else {
     alert("Please select a route!");
   }
 };
 
-watch(
-  () => selectedRoutes.value.length,
-  () => {
-    console.log("selected routes", selectedRoutes.value);
-  }
-);
+// watch(
+//   () => selectedRoutes.value.length,
+//   () => {
+//     console.log("selected routes", selectedRoutes.value);
+//   }
+// );
 
 onMounted(() => {
   demoMap = L.map("map", options);
@@ -169,11 +189,11 @@ onMounted(() => {
       />
       <button
         type="button"
-        class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 self-center flex-initial w-32 flex items-center mt-2"
-        @click="moveMarker"
+        class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 self-center flex-initial w-36 flex items-center mt-2"
+        @click="findRoutes"
       >
         <i class="fa-solid fa-route mr-2" />
-        Simulate
+        Find Routes
       </button>
     </Card>
   </div>
