04fb13632698a30ed19f3cb86a8a9d3bd0b28efe
Özgür Akgün <ozgurakgun@gmail.com>
2024-06-12 14:35:57 +0100

    edit base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
-  base: "https://conjure-cp.github.io/h2a-buses/",
+  base: "/h2a-buses",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
